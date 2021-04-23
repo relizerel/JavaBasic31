d3ebcce9379c8d04aff2151b199e98b4aae8732d
relizerel <relizerel@gmail.com>
2021-04-23 14:42:10 +0200

    add task4

--- src/main/java/by/training/simplestapp/main/lesson1/Task4.java
+++ src/main/java/by/training/simplestapp/main/lesson1/Task4.java
@@ -5,10 +5,15 @@ public class Task4 {
         int cathetus1 = 4;
         int cathetus2 = 2;
         Task4 task4 = new Task4();
-        System.out.println(task4.function(cathetus1, cathetus2));
+        System.out.println(task4.hypotenuseLength(cathetus1, cathetus2));
+        System.out.println(task4.areaTriangle(cathetus1, cathetus2));
     }
 
-    private double function(int cathetus1, int cathetus2){
+    private double hypotenuseLength(int cathetus1, int cathetus2){
         return Math.sqrt(Math.pow(cathetus1, 2) + Math.pow(cathetus2, 2));
+    }
+
+    private double areaTriangle(int cathetus1, int cathetus2){
+        return (cathetus1 * cathetus2) * 0.5;
     }
 }
