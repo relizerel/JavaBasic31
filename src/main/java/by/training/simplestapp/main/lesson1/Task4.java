c58ee36403b83bdbf0022d063416b5c802f08122
relizerel <relizerel@gmail.com>
2021-04-23 14:48:35 +0200

    add task4

--- src/main/java/by/training/simplestapp/main/lesson1/Task4.java
+++ src/main/java/by/training/simplestapp/main/lesson1/Task4.java
@@ -4,9 +4,9 @@ public class Task4 {
     public static void main(String[] args) {
         int cathetus1 = 4;
         int cathetus2 = 2;
-        Task4 task4 = new Task4();
-        System.out.println(task4.hypotenuseLength(cathetus1, cathetus2));
-        System.out.println(task4.areaTriangle(cathetus1, cathetus2));
+        Task4 tempObject = new Task4();
+        System.out.println(tempObject.hypotenuseLength(cathetus1, cathetus2));
+        System.out.println(tempObject.areaTriangle(cathetus1, cathetus2));
     }
 
     private double hypotenuseLength(int cathetus1, int cathetus2){
