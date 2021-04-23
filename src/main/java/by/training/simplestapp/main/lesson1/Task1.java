3bbc6675a612987bcc5f6874e8d809c606eed813
relizerel <relizerel@gmail.com>
2021-04-23 14:02:02 +0200

    add task2

--- src/main/java/by/training/simplestapp/main/lesson1/Task1.java
+++ src/main/java/by/training/simplestapp/main/lesson1/Task1.java
@@ -1,4 +1,4 @@
-package lesson1;
+package by.training.simplestapp.main.lesson1;
 
 public class Task1 {
     public static void main(String[] args) {
---
+++ src/main/java/by/training/simplestapp/main/lesson1/Task2.java
@@ -0,0 +1,9 @@
+package by.training.simplestapp.main.lesson1;
+
+public class Task2 {
+    public static void main(String[] args) {
+        System.out.println("Попов Алексей Константинович");
+        System.out.println("Address");
+        System.out.println("Number Phone");
+    }
+}
